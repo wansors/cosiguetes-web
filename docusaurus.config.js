6b753dc3efbe9b2923f1f0fbfd8d81ea6f681019
Andres Cisneros <wansors@gmail.com>
2025-05-18 22:39:34 +0200

    Initial files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'wansors ', // Usually your GitHub org/user name.
+  organizationName: 'wansors', // Usually your GitHub org/user name.
   projectName: 'cosiguetes-web', // Usually your repo name.
 
   onBrokenLinks: 'throw',
